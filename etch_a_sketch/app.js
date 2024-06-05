783d7e58fe97ba8dd128ae2ed134a9a35e53ac04
emShalaby <galaxian.dev@gmail.com>
2024-06-05 14:48:17 +0300

    the logic of making a gird is in its own function createGrid

--- etch_a_sketch/app.js
+++ etch_a_sketch/app.js
@@ -1,22 +1,28 @@
 const body = document.querySelector("body");
 const main = document.querySelector("#main");
-let container = document.createElement("div");
-container.id = "container";
+const sizeBtn = document.querySelector("#size");
+
 let square = "";
 let row = "";
 
-for (let i = 0; i < 16; i++) {
-  row = document.createElement("div");
-  row.className = "row";
-  for (let j = 0; j < 16; j++) {
-    square = document.createElement("div");
-    square.className = "square";
-    square.addEventListener("mouseover", (x) => {
-      x.currentTarget.style.backgroundColor = "black";
-      console.log("hi");
-    });
-    row.append(square);
+function createGrid() {
+  document.querySelector("#container")?.remove();
+  let container = document.createElement("div");
+  container.id = "container";
+  for (let i = 0; i < 16; i++) {
+    row = document.createElement("div");
+    row.className = "row";
+    for (let j = 0; j < 16; j++) {
+      square = document.createElement("div");
+      square.className = "square";
+      square.addEventListener("mouseover", (x) => {
+        x.currentTarget.style.backgroundColor = "black";
+        console.log("hi");
+      });
+      row.append(square);
+    }
+    container.append(row);
   }
-  container.append(row);
+  main.append(container);
 }
-main.append(container);
+createGrid();
