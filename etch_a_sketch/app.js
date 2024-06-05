066ac3a10b1c67483f0846de5c1305bb08163c75
emShalaby <galaxian.dev@gmail.com>
2024-06-05 14:24:37 +0300

    add hover effect

--- etch_a_sketch/app.js
+++ etch_a_sketch/app.js
@@ -3,11 +3,20 @@ const main = document.querySelector("#main");
 let container = document.createElement("div");
 container.id = "container";
 let square = "";
+let row = "";
+
 for (let i = 0; i < 16; i++) {
+  row = document.createElement("div");
+  row.className = "row";
   for (let j = 0; j < 16; j++) {
     square = document.createElement("div");
     square.className = "square";
-    container.append(square);
+    square.addEventListener("mouseover", (x) => {
+      x.currentTarget.style.backgroundColor = "black";
+      console.log("hi");
+    });
+    row.append(square);
   }
+  container.append(row);
 }
 main.append(container);
