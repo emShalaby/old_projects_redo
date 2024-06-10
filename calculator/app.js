0b6e7a6073a54c79f8b0787a97452a3c5ec3e245
emShalaby <galaxian.dev@gmail.com>
2024-06-10 22:16:48 +0300

    opreate function

--- calculator/app.js
+++ calculator/app.js
@@ -11,3 +11,23 @@ function multiply() {
 function divide() {
   return [...arguments].reduce((total, currentItem) => total / currentItem);
 }
+//recursive function that keeps operating on arguments untill 1 is left then returns it
+function operate(arr) {
+  if (arr.length == 1) return arr[0];
+  //check for operation type
+  if (arr[1] == "+")
+    return operate([add(Number(arr[0]), Number(arr[2]))].concat(arr.slice(3)));
+  else if (arr[1] == "-")
+    return operate(
+      [subtract(Number(arr[0]), Number(arr[2]))].concat(arr.slice(3))
+    );
+  else if (arr[1] == "x")
+    return operate(
+      [multiply(Number(arr[0]), Number(arr[2]))].concat(arr.slice(3))
+    );
+
+  else if (arr[1] == "/")
+    return operate(
+      [divide(Number(arr[0]), Number(arr[2]))].concat(arr.slice(3))
+    );
+}
