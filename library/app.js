6eecee3576865402cdb9ebae19598559af368937
emShalaby <galaxian.dev@gmail.com>
2024-06-14 20:59:46 +0300

    functions that display library books and add books to library

--- library/app.js
+++ library/app.js
@@ -1,15 +1,26 @@
 const myLibrary = ["book1", "book2", "book3"];
 const libraryElement = document.querySelector("#library");
+const newBook = document.querySelector("#newbook");
 
 function Book() {}
-function addBookToLibrary() {}
-function displayBooks() {
-  myLibrary.forEach((element) => {
-    let item = document.createElement("div");
-    let p = document.createElement("p");
-    p.textContent = element;
-    item.append(p);
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  let item = bookToElement(book);
+  libraryElement.append(item);
+}
+function displayLibraryBooks() {
+  myLibrary.forEach((book) => {
+    let item = bookToElement(book);
     libraryElement.append(item);
   });
 }
-displayBooks();
+function bookToElement(book) {
+  let item = document.createElement("div");
+  item.className = "book";
+  let p = document.createElement("p");
+  p.textContent = book;
+  item.append(p);
+  return item;
+}
+
+displayLibraryBooks();
