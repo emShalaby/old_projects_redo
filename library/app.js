4f19b6c62425419273112b80095dace8fb7e7470
emShalaby <galaxian.dev@gmail.com>
2024-06-14 23:34:20 +0300

    some styling and submit button now adds books to library

--- library/app.js
+++ library/app.js
@@ -1,10 +1,21 @@
-const myLibrary = ["book1", "book2", "book3"];
+const myLibrary = [
+  new Book("abc", "abc", "abc"),
+  new Book("abc", "abc", "abc"),
+  new Book("cba", "cba", "cba"),
+];
 const libraryElement = document.querySelector("#library");
 const newBookBtn = document.querySelector("#newbook");
 const modal = document.querySelector("#book_modal");
 const submitBtn = document.querySelector("#submit");
+let author = document.querySelector("#author");
+let title = document.querySelector("#title");
+let pages = document.querySelector("#pages");
 
-function Book() {}
+function Book(author, title, pages) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+}
 function addBookToLibrary(book) {
   myLibrary.push(book);
   let item = bookToElement(book);
@@ -12,15 +23,15 @@ function addBookToLibrary(book) {
 }
 function displayLibraryBooks() {
   myLibrary.forEach((book) => {
-    let item = bookToElement(book);
-    libraryElement.append(item);
+    let element = bookToElement(book);
+    libraryElement.append(element);
   });
 }
-function bookToElement(book) {
+function bookToElement(bookObj) {
   let item = document.createElement("div");
   item.className = "book";
   let p = document.createElement("p");
-  p.textContent = book;
+  p.textContent = bookObj.title;
   item.append(p);
   return item;
 }
@@ -30,6 +41,10 @@ newBookBtn.addEventListener("click", () => {
   modal.className = "on";
 });
 submitBtn.addEventListener("click", (event) => {
-  modal.className = "off";
   event.preventDefault();
+
+  let newBook = new Book(author.value, title.value, pages.value);
+  let newBookElement = bookToElement(newBook);
+  libraryElement.append(newBookElement);
+  modal.className = "off";
 });
