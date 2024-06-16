cbeec5be58dfb14612abd1d8cbafb4290ab1eef9
emShalaby <galaxian.dev@gmail.com>
2024-06-16 04:35:19 +0300

    working game in console

---
+++ tic_tac_toe/app.js
@@ -0,0 +1,37 @@
+function createPlayer(name, value) {
+  return { name, value };
+}
+
+function game(
+  name1 = "player1",
+  name2 = "player2",
+  value1 = "x",
+  value2 = "o"
+) {
+  let board = ["", "", "", "", "", "", "", "", ""];
+  let turn = 1;
+  let player1 = createPlayer(name1, value1);
+  let player2 = createPlayer(name2, value2);
+  let playerToGo = player1;
+  function checkWinner() {
+    if (
+      ((board[0] == board[1]) == board[2]) != "" ||
+      ((board[3] == board[4]) == board[5]) != "" ||
+      ((board[6] == board[7]) == board[8]) != "" ||
+      ((board[0] == board[4]) == board[8]) != "" ||
+      ((board[2] == board[4]) == board[6]) != "" ||
+      ((board[0] == board[3]) == board[6]) != "" ||
+      ((board[1] == board[4]) == board[7]) != "" ||
+      ((board[2] == board[5]) == board[8]) != ""
+    ) {
+      console.log(playerToGo.name);
+    }
+  }
+  function add(index) {
+    board[index] = playerToGo.value;
+    checkWinner();
+    turn++;
+    playerToGo = turn % 2 ? player1 : player2;
+  }
+  return { board, add };
+}
