a50dd78b0c384d0c0ccbdfea550c2ba47e5bd25b
emShalaby <medo.dice@gmail.com>
2024-06-26 08:16:57 +0300

    checkwinner now works properly

--- tic_tac_toe/app.js
+++ tic_tac_toe/app.js
@@ -15,14 +15,14 @@ function game(
   let playerToGo = player1;
   function checkWinner() {
     if (
-      ((board[0] == board[1]) == board[2]) != "" ||
-      ((board[3] == board[4]) == board[5]) != "" ||
-      ((board[6] == board[7]) == board[8]) != "" ||
-      ((board[0] == board[4]) == board[8]) != "" ||
-      ((board[2] == board[4]) == board[6]) != "" ||
-      ((board[0] == board[3]) == board[6]) != "" ||
-      ((board[1] == board[4]) == board[7]) != "" ||
-      ((board[2] == board[5]) == board[8]) != ""
+      (board[0] == board[1] && board[0] == board[2] && board[0] != "") ||
+      (board[3] == board[4] && board[3] == board[5] && board[3] != "") ||
+      (board[6] == board[7] && board[6] == board[8] && board[6] != "") ||
+      (board[0] == board[4] && board[0] == board[8] && board[0] != "") ||
+      (board[2] == board[4] && board[2] == board[6] && board[2] != "") ||
+      (board[0] == board[3] && board[0] == board[6] && board[0] != "") ||
+      (board[1] == board[4] && board[1] == board[7] && board[1] != "") ||
+      (board[2] == board[5] && board[2] == board[8] && board[2] != "")
     ) {
       console.log(playerToGo.name);
     }
