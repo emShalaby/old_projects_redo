1bafb9c024d7e68ddac548bdaf95c25fceb67026
emShalaby <medo.dice@gmail.com>
2024-07-02 23:02:55 +0300

    initial commit for todo list project

---
+++ todo_list/webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant Page",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: "asset/resource",
+      },
+    ],
+  },
+};
