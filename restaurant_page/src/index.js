527e1b975a48224d8ad7f4f51e4b244c2d07775c
emShalaby <medo.dice@gmail.com>
2024-07-02 17:05:50 +0300

    buttons light up when they are clicked

--- restaurant_page/src/index.js
+++ restaurant_page/src/index.js
@@ -1,4 +1,20 @@
 import "./style.css";
 import homeContent from "./home";
 
+const homeBtn = document.querySelector("#home");
+const menuBtn = document.querySelector("#menu");
+const aboutBtn = document.querySelector("#about");
+const content = document.querySelector("#content");
+const home = { btn: homeBtn, content: homeContent };
+const menu = { btn: menuBtn };
+const about = { btn: aboutBtn };
+const pages = [home, menu, about];
+pages.forEach((page) =>
+  page.btn.addEventListener("click", () => {
+    pages.forEach((page) => (page.btn.className = "off"));
+    page.btn.className = "on";
+    content.textContent = "";
+  })
+);
+homeBtn.className = "on";
 homeContent();
