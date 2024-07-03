6a6fb7ee6434fe3030f262b18eb6784dee1d3231
emShalaby <medo.dice@gmail.com>
2024-07-03 10:16:24 +0300

    button and basic modal for adding a new task

--- todo_list/src/index.js
+++ todo_list/src/index.js
@@ -2,6 +2,7 @@ import task from "./todo.js";
 import project from "./project.js";
 import store from "./store";
 import inbox from "./inbox.js";
+import "./style.css";
 window.task = task;
 window.project = project;
 window.store = store;
