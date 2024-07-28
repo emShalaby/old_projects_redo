f7ce727565c895eeee5006ea8986943b370d5f44
emShalaby <medo.dice@gmail.com>
2024-07-28 19:05:58 +0300

    form styling and tasklist styling

--- todo_list/src/DOM/DOM_project.js
+++ todo_list/src/DOM/DOM_project.js
@@ -6,26 +6,51 @@ export default function populateContent(project = inbox) {
   const h2 = document.createElement("h2");
   h2.id = "content-project-name";
   const tasksElement = document.createElement("div");
+  const col = document.createElement("div");
+  tasksElement.classList.add("row");
+  tasksElement.classList.add("justify-content-center");
+  col.classList.add("col");
+  col.classList.add("col-8");
+
   for (let i = 0; i < project.tasks.length; i++) {
     const task = document.createElement("div");
+    const taskBody = document.createElement("div");
     const name = document.createElement("h4");
     const description = document.createElement("p");
     const dueDate = document.createElement("p");
     const taskObj = store.tasks.get(project.tasks[i]);
+    const btnGroup = document.createElement("div");
     const editTaskBtn = document.createElement("button");
+    const deleteTaskBtn = document.createElement("button");
+
+    task.classList.add("card");
+    taskBody.classList.add("card-body");
     editTaskBtn.textContent = "Edit Task";
     editTaskBtn.classList.add("btn");
     editTaskBtn.classList.add("btn-secondary");
-    editTaskBtn.classList.add("btn-sm")
-    description.textContent = taskObj.description;
+    editTaskBtn.classList.add("btn-sm");
+    deleteTaskBtn.classList.add("btn");
+    deleteTaskBtn.classList.add("btn-danger");
+    deleteTaskBtn.classList.add("btn-sm");
+    deleteTaskBtn.classList.add("ms-2");
+    deleteTaskBtn.textContent = "Delete";
     name.textContent = taskObj.name;
+    name.classList.add("card-title");
+    description.textContent = taskObj.description;
+    description.classList.add("card-text");
     dueDate.textContent = taskObj.dueDate;
-    task.append(name);
-    task.append(description);
-    task.append(dueDate);
-    tasksElement.append(task);
-    tasksElement.append(editTaskBtn);
+    taskBody.append(name);
+    taskBody.append(description);
+    taskBody.append(dueDate);
+    task.append(taskBody);
+
+    btnGroup.append(editTaskBtn);
+    btnGroup.append(deleteTaskBtn);
+    btnGroup.classList.add("p-3");
+    taskBody.append(btnGroup);
+    col.append(task);
   }
+  tasksElement.append(col);
   tasksElement.id = "current-tasks";
   h2.textContent = project.name;
   currentProject.append(h2);
