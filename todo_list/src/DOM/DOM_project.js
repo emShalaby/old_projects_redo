94a15c3916ac2d194de3921049cf1948bdbb8598
emShalaby <medo.dice@gmail.com>
2024-07-28 16:33:58 +0300

    add edit btn

--- todo_list/src/DOM/DOM_project.js
+++ todo_list/src/DOM/DOM_project.js
@@ -7,11 +7,16 @@ export default function populateContent(project = inbox) {
   h2.id = "content-project-name";
   const tasksElement = document.createElement("div");
   for (let i = 0; i < project.tasks.length; i++) {
-    let task = document.createElement("div");
-    let name = document.createElement("h4");
-    let description = document.createElement("p");
-    let dueDate = document.createElement("p");
-    let taskObj = store.tasks.get(project.tasks[i]);
+    const task = document.createElement("div");
+    const name = document.createElement("h4");
+    const description = document.createElement("p");
+    const dueDate = document.createElement("p");
+    const taskObj = store.tasks.get(project.tasks[i]);
+    const editTaskBtn = document.createElement("button");
+    editTaskBtn.textContent = "Edit Task";
+    editTaskBtn.classList.add("btn");
+    editTaskBtn.classList.add("btn-secondary");
+    editTaskBtn.classList.add("btn-sm")
     description.textContent = taskObj.description;
     name.textContent = taskObj.name;
     dueDate.textContent = taskObj.dueDate;
@@ -19,6 +24,7 @@ export default function populateContent(project = inbox) {
     task.append(description);
     task.append(dueDate);
     tasksElement.append(task);
+    tasksElement.append(editTaskBtn);
   }
   tasksElement.id = "current-tasks";
   h2.textContent = project.name;
