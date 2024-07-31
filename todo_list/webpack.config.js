716eaadb43ef4514aa61230c894392efb285d905
emShalaby <medo.dice@gmail.com>
2024-07-31 04:49:26 +0300

    add material-ui icons

--- todo_list/webpack.config.js
+++ todo_list/webpack.config.js
@@ -42,10 +42,11 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|jpg|jpeg|gif)$/i,
 
         type: "asset/resource",
       },
+      { test: /\.svg$/, use: ["svg-inline-loader"] },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
 
